97472dee86b3f9f50de22a0ac31c124e54412a97
rodrigues882013 <rodrigues882007@gmail.com>
2017-12-20 20:53:27 -0200

    --first commit

---
+++ src/main/java/com/techtest/vhforum/VhForumApplication.java
@@ -0,0 +1,12 @@
+package com.techtest.vhforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VhForumApplication.class, args);
+	}
+}
